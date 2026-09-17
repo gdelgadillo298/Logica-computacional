e2a0225e8859e4897c400f49d30c21653983cc0c
gdelgadillo298 <gdelgadillo298@uan.edu.co>
2026-09-17 13:51:46 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+    {
+        int numero,resultado;
+        cout<<"Ingresa un numero entero: ";
+        cin>>numero;
+        resultado=numero*2;
+        cout<<"El resultado es: "<<resultado<<endl;
+    }
